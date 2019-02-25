86e4a956947e625076c10a5828bd4c5513983f70
Shreya Sheel <ssheel@ucsd.edu>
2019-02-25 15:17:58 -0800

    should work?

--- app.js
+++ app.js
@@ -8,9 +8,25 @@ var http = require('http');
 var path = require('path');
 var handlebars = require('express3-handlebars')
 
-var index = require('./routes/index');
+//var index = require('./routes/index');
+//var project = require('./routes/project');
+
+var index = require('./routes/index')
+ var searchresults = require('./routes/searchresultsJS');
+var messages = require('./routes/message')
+var myprofile = require('./routes/myprofileJS')
+var search = require('./routes/searchJS')
+var createaccount = require('./routes/createaccountJS')
+
+//mentor pages
+// var mentor_abigail = require('./routes/mentor-abigailJS')
+// var mentor_elliott = require('./routes/mentor-elliottJS')
+// var mentor_kiefer = require('./routes/mentor-kieferJS')
+// var mentor_mateo = require('./routes/mentor-mateoJS')
+// var mentor_heather = require('./routes/mentor-heatherJS')
+
 // Example route
-// var user = require('./routes/user');
+//var user = require('./routes/user');
 
 var app = express();
 
@@ -35,7 +51,23 @@ if ('development' == app.get('env')) {
 }
 
 // Add routes here
-app.get('/', index.view);
+app.get('/', index.view)
+ app.get('/searchresults', searchresults.view);
+ app.get('/messages', messages.view)
+ app.get('/myprofile', myprofile.view);
+ app.get('/search', search.view)
+ app.get('/createaccount', createaccount.view)
+
+
+//routes for mentors
+// app.get('/searchresults/mentor_abigail', mentor_abigail.view)
+// app.get('/searchresults/mentor_elliott', mentor_elliott.view)
+// app.get('/searchresults/mentor_kiefer', mentor_kiefer.view)
+// app.get('/searchresults/mentor_mateo', mentor_mateo.view)
+// app.get('/searchresults/mentor_heather', mentor_heather.view)
+
+
+//app.get('/project/:id', project.projectInfo);
 // Example route
 // app.get('/users', user.list);
 
--- routes/add.js
+++
@@ -1,5 +0,0 @@
-var data = require("../data.json");
-
-exports.addFriend = function(request, response) {    
-	// Your code goes here
- }
