7ababa9a724b6b59c03c102b5b15e7ff18adafde
Jordynn Bartolome <jordynnbartolome@gmail.com>
2019-02-26 18:15:58 -0800

    Google analytics?

---
+++ public/js/analytics.js
@@ -0,0 +1,20 @@
+'use strict';
+
+// Call this function when the page loads (the "ready" event)
+$(document).ready(function() {
+	initializePage();
+});
+
+/*
+ * Function that is called when the document is ready.
+ */
+function initializePage() {
+	// your code here
+	$("a").click(projectClick);
+	
+}
+
+function projectClick(e) {
+	e.preventDefault();
+	ga("send", "event",'name', 'click');
+}
