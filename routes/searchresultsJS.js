4a864084b902099167fb1d96696412fc124ea0b5
Jordynn Bartolome <jordynnbartolome@gmail.com>
2019-03-04 15:50:21 -0800

    PLZ WORK

--- routes/searchresultsJS.js
+++ routes/searchresultsJS.js
@@ -1,9 +1,11 @@
 // Get all of our friend data
 var data = require('../data.json');
+// var filter = {};
 
 exports.view = function(request, response){
 	console.log(data);
 	data["viewAlt"]=false;
+	//Return partial data only the one that is filtered. Return a different variable
 	response.render('searchresults', data);
 };
 
@@ -12,3 +14,5 @@ exports.viewAlt = function(request, response){
 	data["viewAlt"]=true;
 	response.render('searchresults', data);
 }
+
+//Create function that sets variable filter. Then create 
